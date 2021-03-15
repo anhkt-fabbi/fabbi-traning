e6f9f8c0c572e63f9b615eed42c1990db702fb89
AnhKT-F1-D2 <anhkt@fabbi.io>
2021-03-15 15:00:07 +0700

    fix api

--- app/Repositories/User/UserRepository.php
+++ app/Repositories/User/UserRepository.php
@@ -251,7 +251,7 @@ class UserRepository extends RepositoryAbstract implements UserRepositoryInterfa
         $user = $this->model->findOrFail($id);
 
         $data = Vote::with(['options' => function ($q) {
-            $q->withCount('users as qtyVote');
+            $q->with('listUpVote:id,full_name as fullName,email');
         }])->where('user_id', $user->id);
         if (!empty($request['title'])) {
             $data->where('title', 'like', $request['title']);
